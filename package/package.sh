6b665a8a584f7a3e981baffa35addcb546ef3aa1
Antonio Galea <antonio.galea@gmail.com>
2017-02-20 18:38:42 +0100

    packaging aids, draft

---
+++ package/package.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -ex
+
+VERSION=`grep version= ../platform.txt | sed 's/version=//g'`
+FILENAME="yarm-arduino-${VERSION}.tar.bz2"
+
+tar --transform "s|\.|saml|" \
+  --exclude=IDE_Board_Manager \
+  --exclude=.git* \
+  --exclude=.idea \
+  --exclude=.keep \
+  --exclude=package/** \
+  -cjf ${FILENAME} \
+  -C .. \
+  .
+
+CHECKSUM=`sha256sum $FILENAME | awk '{ print $1 }'`
+SIZE=`wc -c $FILENAME | awk '{ print $1 }'`
+
+cat package_yarm_index.json.template |
+sed s/%%VERSION%%/${PACKAGE_VERSION}/ |
+sed s/%%FILENAME%%/${FILENAME}/ |
+sed s/%%CHECKSUM%%/${CHKSUM}/ |
+sed s/%%SIZE%%/${SIZE}/ > package_yarm_index.json
+
+mv ${FILENAME} package_yarm_index.json ../IDE_Board_Manager/
